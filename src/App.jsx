76d60cb1520193ef74e4770fca3c3bb8a6e9bc00
Gaurav Rawat <37866054+GauravRawat01@users.noreply.github.com>
2023-06-01 02:10:22 +0530

    checking in the initial UI code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import "./App.css";
 import ToDoList from "./components/ToDoList";
 
+const useStyles = makeStyles(() => ({
+  titleBox: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appTitle: {
+    fontSize: "3rem",
+    textAlign: "center",
+    marginBottom: 0,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <>
+      <Box className={classes.titleBox}>
+        <h1 className={classes.appTitle}>Simple React TODO List</h1>
+        <sub>powered by Material UI</sub>
+      </Box>
       <ToDoList />
     </>
   );
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,9 +1,41 @@
-import react from "react";
+import React, { useState } from "react";
+import { Box, TextField, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  listBox: {
+    display: "flex",
+    marginLeft: "auto",
+    marginRight: "auto",
+    alignItems: "center",
+    height: "35rem",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  todoInputTextfield: {
+    width: "30rem",
+    marginTop: "2rem",
+    marginBottom: "2rem",
+  },
+  addButton: {
+    backgroundColor: "gray",
+  },
+}));
 
 function ToDoList() {
+  const classes = useStyles();
+  const [todoList, setToDoList] = useState([]);
+
   return (
     <>
-      <div>Hello World</div>
+      <Box className={classes.listBox}>
+        <TextField
+          className={classes.todoInputTextfield}
+          label="Enter TODO description to add to the list."
+          id="todoInput"
+        />
+        <Button className={classes.addButton}>Add to list</Button>
+      </Box>
     </>
   );
 }
